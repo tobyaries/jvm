7d00665ae0282ad1f5dfb3750783d78dc1a29d2f
tobyaries <yestin.tian@qq.com>
2020-08-07 14:13:13 +0800

    Initial commit

---
+++ src/jvm/classloader/MyTest7.java
@@ -0,0 +1,15 @@
+package jvm.classloader;
+
+public class MyTest7 {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class<?> clazz = Class.forName("java.lang.String");
+        System.out.println(clazz.getClassLoader());
+
+        Class<?> clazz2 = Class.forName("jvm.classloader.C");
+        System.out.println(clazz2.getClassLoader());
+    }
+}
+
+class C {
+
+}
